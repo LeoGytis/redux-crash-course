b48d9a8de7c346131b8ffb0a089055fd6247fbc0
Gytis <101097792+LeoGytis@users.noreply.github.com>
2022-11-29 08:35:12 +0200

    able to run

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://www.youtube.com/watch?v=93p3LxR9xfM&ab_channel=TraversyMedia
 
 Code For This Project:
 https://github.com/bradtraversy/redux...
+
+Commands:
+npm i redux react-redux redux-thunk
--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
+import { Provider } from "react-redux";
+// import store from "./store";
+import { applyMiddleware, createStore } from "redux";
 
 function App() {
+  const store = createStore(() => [], {}, applyMiddleware());
+
   return (
-    <div className="App">
-      <Postform />
-      <Posts></Posts>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Postform></Postform>
+        <Posts></Posts>
+      </div>
+    </Provider>
   );
 }
 
